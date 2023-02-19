c0c1ff3aab5012d06a157816625335de0c72afb2
osawa-koki <github@osawa.dev>
2023-02-20 03:17:17 +0900

    雛形。

--- src/Program.vb
+++ src/Program.vb
@@ -1,7 +1,16 @@
-Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
+  Sub Main()
+
+    ' 配列を宣言
+    Dim nums As Integer() = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    ' 配列の要素を表示
+    Display("Original", nums)
+
+    ' 配列をシャッフル
+    Shuffle(nums)
+    Display("Shuffled", nums)
+
   End Sub
 End Module
---
+++ src/Shuffle.vb
@@ -0,0 +1,15 @@
+﻿
+Module ShuffleModule
+  Sub Shuffle(ByRef nums As Array)
+
+    ' Fisher-Yatesアルゴリズムによる配列のシャッフル
+    Dim rand As New Random()
+    For i As Integer = nums.Length - 1 To 1 Step -1
+      Dim j As Integer = rand.Next(i + 1)
+      Dim temp As Integer = nums(i)
+      nums(i) = nums(j)
+      nums(j) = temp
+    Next
+
+  End Sub
+End Module
